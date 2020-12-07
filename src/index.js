3b0b4dfcd86d6cfd54684d6f1e42968ae95fc22b
kinha08 <lucasnobredasilva@gmail.com>
2020-12-07 11:44:38 -0300

    change board hardcode to a loop

--- src/index.js
+++ src/index.js
@@ -37,27 +37,50 @@ class Board extends React.Component {
         onClick={() => this.props.onClick(i)}
       />
     );
-  }
+	}
 
+	renderRow(row) {
+		//const board = []
+		const squares = []
+		const offset = row * 3
+		for(let i = 0; i < 3 ; i++) {
+			squares.push(this.renderSquare(offset + i));
+		}
+
+		return (
+			<div className="board-row">
+				{squares}
+			</div>
+		);
+	}
   render() {
+		const rows = []
+		for (let j = 0; j < 3; j++) {
+			rows.push(this.renderRow(j));
+		}
+
     return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+			<div className="board">
+				{rows}
+			</div>
+
+    //   <div className="board">
+    //     <div className="board-row">
+    //       {this.renderSquare(0)}
+    //       {this.renderSquare(1)}
+    //       {this.renderSquare(2)}
+    //     </div>
+    //     <div className="board-row">
+    //       {this.renderSquare(3)}
+    //       {this.renderSquare(4)}
+    //       {this.renderSquare(5)}
+    //     </div>
+    //     <div className="board-row">
+    //       {this.renderSquare(6)}
+    //       {this.renderSquare(7)}
+    //       {this.renderSquare(8)}
+    //     </div>
+    //   </div>
     );
   }
 }
